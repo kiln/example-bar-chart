401e0ff8fe94d7064d88cee9d74d36f8bebd4a67
daanlouter <daan.louter@gmail.com>
2019-07-21 20:59:53 +0100

    Update Flourish and packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-var nodeResolve = require("rollup-plugin-node-resolve"),
-    uglify = require("rollup-plugin-uglify");
+import nodeResolve from "rollup-plugin-node-resolve";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "src/index.js",
@@ -12,5 +12,17 @@ export default {
   plugins: [
     nodeResolve(),
     uglify(),
-  ]
+  ],
+  onwarn: function (warning, warn) {
+    if (warning.code === "CIRCULAR_DEPENDENCY") return;
+    if (warning.code === "UNRESOLVED_IMPORT") {
+      throw new Error(
+        "Couldn't resolve the dependency " + warning.source +
+          " (from " + warning.importer + "): sometimes you can" +
+          " fix this with 'npm install', or add '" + warning.source +
+          " to 'external'. See: " + warning.url
+      );
+    }
+    warn(warning);
+  }
 };
